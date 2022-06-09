d7480aca6fb9a2e65cf7f13223e24d6d5bc97077
owentr1369 <tamvaa1306@gmail.com>
2022-06-09 16:31:36 +0700

    d when hold a

--- index.js
+++ index.js
@@ -67,6 +67,8 @@ const keys = {
   },
 };
 
+let lastKey;
+
 function animate() {
   window.requestAnimationFrame(animate);
   c.fillStyle = "black";
@@ -75,9 +77,9 @@ function animate() {
   enemy.update();
   player.velocity.x = 0;
 
-  if (keys.a.pressed) {
+  if (keys.a.pressed && lastKey === "a") {
     player.velocity.x = -1;
-  } else if (keys.d.pressed) {
+  } else if (keys.d.pressed && lastKey === "d") {
     player.velocity.x = 1;
   }
 }
@@ -88,9 +90,11 @@ window.addEventListener("keydown", (event) => {
   switch (event.key) {
     case "d":
       keys.d.pressed = true;
+      lastKey = "d";
       break;
     case "a":
       keys.a.pressed = true;
+      lastKey = "a";
       break;
   }
   console.log(event.key);
@@ -100,9 +104,11 @@ window.addEventListener("keyup", (event) => {
   switch (event.key) {
     case "d":
       keys.d.pressed = false;
+
       break;
     case "a":
       keys.a.pressed = false;
+
       break;
   }
   console.log(event.key);
