94ddabd2eb28d7aee595f1a289c0befa06e6fa01
owentr1369 <tamvaa1306@gmail.com>
2022-06-09 16:21:52 +0700

    move forward and backward

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ class Sprite {
   }
   update() {
     this.draw();
+    this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
 
     if (this.position.y + this.height + this.velocity.y >= canvas.height) {
@@ -66,3 +67,27 @@ function animate() {
 }
 
 animate();
+
+window.addEventListener("keydown", (event) => {
+  switch (event.key) {
+    case "d":
+      player.velocity.x = 1;
+      break;
+    case "a":
+      player.velocity.x = -1;
+      break;
+  }
+  console.log(event.key);
+});
+
+window.addEventListener("keyup", (event) => {
+  switch (event.key) {
+    case "d":
+      player.velocity.x = 0;
+      break;
+    case "a":
+      player.velocity.x = 0;
+      break;
+  }
+  console.log(event.key);
+});
