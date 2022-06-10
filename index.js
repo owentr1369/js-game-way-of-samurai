d19402dde7fcc59e3a5f8ad89f48ad6f1452c361
owentr1369 <tamvaa1306@gmail.com>
2022-06-10 14:51:30 +0700

    completed with background and fighters position

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ const shop = new Sprite({
 
 const player = new Fighter({
   position: {
-    x: 0,
+    x: 168,
     y: 0,
   },
   velocity: {
@@ -90,7 +90,7 @@ const player = new Fighter({
 player.draw();
 const enemy = new Fighter({
   position: {
-    x: 400,
+    x: 800,
     y: 100,
   },
   velocity: {
