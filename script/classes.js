fd8ca1eb8d8d28c9d60a948e925269284d769feb
owentr1369 <tamvaa1306@gmail.com>
2022-06-10 10:28:46 +0700

    background sprite

--- script/classes.js
+++ script/classes.js
@@ -40,7 +40,7 @@ class Fighter {
     this.position.x += this.velocity.x;
     this.position.y += this.velocity.y;
 
-    if (this.position.y + this.height + this.velocity.y >= canvas.height) {
+    if (this.position.y + this.height + this.velocity.y >= canvas.height - 96) {
       this.velocity.y = 0;
     } else {
       this.velocity.y += gravity;
