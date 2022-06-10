9abb23b790491ebdf2852c4952ce9f56e34c622c
owentr1369 <tamvaa1306@gmail.com>
2022-06-10 14:41:52 +0700

    interface design and animation

--- index.js
+++ index.js
@@ -179,6 +179,8 @@ function animate() {
 
   background.update();
   shop.update();
+  c.fillStyle = "rgba(255,255,255, 0.15";
+  c.fillRect(0, 0, canvas.width, canvas.height);
   player.update();
   enemy.update();
 
@@ -237,7 +239,9 @@ function animate() {
     enemy.takeHit();
     player.isAttacking = false;
 
-    document.querySelector("#enemyHealth").style.width = enemy.health + "%";
+    gsap.to("#enemyHealth", {
+      width: enemy.health + "%",
+    });
   }
   // if player misses
   if (player.isAttacking && player.frameCurrent === 4) {
@@ -254,8 +258,10 @@ function animate() {
   ) {
     player.takeHit();
     enemy.isAttacking = false;
-    player.health -= 20;
-    document.querySelector("#playerHealth").style.width = player.health + "%";
+
+    gsap.to("#playerHealth", {
+      width: player.health + "%",
+    });
   }
 
   // If enemy misses
